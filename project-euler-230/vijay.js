27b59083bd22ec9a3d3b5f5de97dde2ff4dfb7f1
Vijay <vijaykrishna@mroads.com>
2019-10-27 11:59:30 +0530

    updated code for project-euler-230

--- project-euler-230/vijay.js
+++ project-euler-230/vijay.js
@@ -4,8 +4,8 @@ const BigNumber = require('bignumber.js');
 
 /**
  *
-2
-1415926535 8979323846 35
+1
+1415926535 8979323846 21
 1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679 8214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196 104683731294243150
 
 */
@@ -13,40 +13,29 @@ const BigNumber = require('bignumber.js');
 let iterationValues = [];
 
 
-function calculateDifference(val1, val2) {
-  return new BigNumber(`${val1}`).minus(val2);
-}
-
-
 function findIteration(n, A, B, iteration) {
-  const AB = A + B;
-
   // console.info('iteration', n, A, B, iteration);
 
 
-  if (iteration === 2 && n <= AB.length) {
-    // console.info('match found', iteration, n, AB);
-    return AB[n > 0 ? n - 1 : n];
-  }
-
-
-  for (let i = iteration; i > 0; i--) {
-    // console.info('find me', iterationValues[i], n);
+  for (let i = iteration; i >= 0; i -= 2) {
+    // console.info('find me', iterationValues[i], n, i);
     if (n.minus(iterationValues[i]) > 0) {
       const difference = n.minus(iterationValues[i]);
-      // console.info('n, iterationValues[i]', n, iterationValues[i], i, difference);
-      return findIteration(difference, A, B, i);
+      // console.info('difference', i, iterationValues[i]);
+      if (i > 0 && difference.minus(iterationValues[i - 1]) > 0) {
+        // console.info('in second part', difference.minus(iterationValues[i - 1]));
+        return findIteration(difference.minus(iterationValues[i - 1]), A, B, i);
+      }
+      // console.info('in first part', difference.minus(iterationValues[i - 1]));
+
+      return findIteration(difference, A, B, i - 1);
     }
   }
 
-  if (n < B.length) {
-    // console.info('match found', iteration, n, AB);
-    return B[n > 0 ? n - 1 : n];
-  } if (n < AB.length) {
-    // console.info('match found', iteration, n, AB);
-    return AB[n > 0 ? n - 1 : n];
+  if (iteration % 2 === 0) {
+    return A[n > 0 ? n - 1 : n];
   }
-  // return B[n - 1];
+  return B[n > 0 ? n - 1 : n];
 }
 
 
@@ -54,12 +43,29 @@ function processData(input) {
   // Enter your code here
   const { q, data } = extractData(input);
   // console.info('q,data', q, data.length);
-  data.forEach((line) => {
+
+  for (let i = 0; i < q; i++) {
+    const line = data[i];
     // console.info('line', line);
     iterationValues = [];
     const { A, B, n } = line;
+
+    if (n <= A.length) {
+      console.info(A[n - 1]);
+      break;
+    }
+
+    const AB = A + B;
+
+    if (n <= AB.length) {
+      console.info(AB[n - 1]);
+      break;
+    }
+
+
     let prev1Length = new BigNumber(A.length);
     let prev2Length = new BigNumber(B.length);
+    iterationValues.push(prev1Length);
     iterationValues.push(prev2Length);
     while (true) {
       try {
@@ -71,24 +77,23 @@ function processData(input) {
           break;
         }
       } catch (e) {
-        console.warn(e);
+        console.warn('error', e);
         break;
       }
     }
 
-    // console.info('iterationValues', iterationValues, iterationValues.length - 1);
+    // console.info('iterationValues', iterationValues.length - 1, n.minus(prev1Length));
+
 
     const value = findIteration(n, A, B, iterationValues.length - 1);
-
     console.info(value);
-  });
+  }
 }
 
 
 function extractData(input) {
-  const inputLines = input.split('\n');
+  const inputLines = input.trim().split('\n');
   if (inputLines.length < 2) {
-    console.info(0);
     return {
       q: 0,
     };
@@ -125,8 +130,6 @@ process.stdin.on('data', (input) => {
 });
 
 process.stdin.on('end', () => {
+  // console.info('ended');
   processData(_input);
 });
-
-
-// processData('1\n1415926535 8979323846 20');
