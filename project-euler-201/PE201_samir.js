c87b9621f8191f2b766c93fd0a0e7f6de3bce8f8
Vijay <vijaykrishna@mroads.com>
2019-11-27 18:35:46 +0530

    fixed your code

--- project-euler-201/PE201_samir.js
+++ project-euler-201/PE201_samir.js
@@ -1,61 +1,94 @@
-const subsetSumArray = [];
+
+const uniqueSum = {};
 
 function getSubsetsSumArray(arr, al, sl, index, data, i) {
-    if (index === +sl) {
-        let sum = 0;
-        for (j = 0; j < sl; j++) {
-            sum += +data[j];
-        }
-        subsetSumArray.push(sum);
-        return; 
+  if (index === +sl) {
+    let sum = 0;
+    for (let j = 0; j < sl; j++) {
+      sum += +data[j];
     }
-    if (i >= al) {
-        return;
+    if (uniqueSum[sum]) {
+      uniqueSum[sum] += 1;
+    } else {
+      uniqueSum[sum] = 1;
     }
-    data[index] = arr[i];
-    getSubsetsSumArray(arr, al, sl, index + 1, data, i + 1); 
-    getSubsetsSumArray(arr, al, sl, index, data, i + 1); 
+    return;
+  }
+  if (i >= al) {
+    return;
+  }
+  data[index] = arr[i];
+  getSubsetsSumArray(arr, al, sl, index + 1, data, i + 1);
+  getSubsetsSumArray(arr, al, sl, index, data, i + 1);
 }
 
 function getUniqueSumArray(inputArray) {
-    const uArray = [];
-    inputArray.forEach(ele => {
-        const eleIndex = uArray.indexOf(ele);
-         eleIndex === -1 ? 
-            uArray.push(ele) : 
-            uArray.splice(eleIndex, 1);
-       
-    });
-    return uArray;
+  const uArray = [];
+  inputArray.forEach((ele) => {
+    const eleIndex = uArray.indexOf(ele);
+    eleIndex === -1
+      ? uArray.push(ele)
+      : uArray.splice(eleIndex, 1);
+  });
+  return uArray;
 }
 
 function processData(input) {
-    const subsetLength = input.split('\n')[0].split(' ')[1];
-    const arrLen = input.split('\n')[0].split(' ')[0];
-    let splitArr = input.split('\n')[1].split(' ');
+  const subsetLength = input.split('\n')[0].split(' ')[1];
+  const arrLen = input.split('\n')[0].split(' ')[0];
+  const splitArr = input.split('\n')[1].split(' ');
 
-    if (arrLen < splitArr.length) {
-        splitArr.splice(0, arrLen);
+  if (arrLen < splitArr.length) {
+    splitArr.splice(0, arrLen);
+  }
+  if (subsetLength > splitArr.length) {
+    return 0;
+  }
+  getSubsetsSumArray(splitArr, arrLen, subsetLength, 0, [], 0);
+  let total = 0;
+  Object.keys(uniqueSum).forEach((sum) => {
+    if (uniqueSum[sum] === 1) {
+      total += +sum;
     }
-    if (subsetLength > splitArr.length) {
-        return 0;
-    }
-    getSubsetsSumArray(splitArr, arrLen, subsetLength, 0, [], 0);
-//     getSubsetsSumArray([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
-// , 18, 19, 20, 21 ] , 21, 9, 0, [], 0);
-    // console.info('subsetSumArray', subsetSumArray.length);
-    const uniqueArray = getUniqueSumArray(subsetSumArray);
-    return uniqueArray.reduce((total, i) => total + i, 0);
-} 
+  });
+  // const uniqueArray = getUniqueSumArray(subsetSumArray);
+  // console.info('uniqueArray', Object);
 
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-_input = "";
-process.stdin.on("data", function (input) {
-    _input += input;
-});
+  // const total = uniqueSumArray.reduce((s, i) => s + i, 0);
 
-process.stdin.on("end", function () {
-   var result = processData(_input);
-   console.info(result);
+  // console.info('total', total, new BigNumber(total));
+  return total;
+}
+
+// process.stdin.resume();
+// process.stdin.setEncoding('ascii');
+// _input = '';
+// process.stdin.on('data', (input) => {
+//   _input += input;
+// });
+
+// process.stdin.on('end', () => {
+//   const result = processData(_input);
+//   console.info(result);
+// });
+
+
+const { data, m, n } = {
+  data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 29], m: 15, numOfSubSets: '77558759.99999999', n: 29,
+};
+
+const startTime = new Date().getTime();
+
+getSubsetsSumArray(data, n, m, 0, [], 0);
+let total = 0;
+Object.keys(uniqueSum).forEach((sum) => {
+  if (uniqueSum[sum] === 1) {
+    total += +sum;
+  }
 });
+// const uniqueArray = getUniqueSumArray(subsetSumArray);
+// console.info('uniqueArray', Object);
+
+// const total = uniqueSumArray.reduce((s, i) => s + i, 0);
+
+console.info('total', total, 'executed in ', new Date().getTime() - startTime);
